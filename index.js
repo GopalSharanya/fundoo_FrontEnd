727e5567915727a014717d287dd34b80d87d8f65
SharanyaGopal <sharanyagopal97@gmail.com>
2020-03-30 20:26:01 +0530

    [sharanya] added dashboard

--- index.js
+++ index.js
@@ -48,12 +48,24 @@ app.get('/dashboard.html', function (req, res) {
     res.sendFile(path.join(__dirname + '/components/dashboard.html'));
 });
 
+app.get('/one.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/components/one.html'));
+});
+
 app.get('/one.jpeg', function (req, res) {
     res.sendFile(path.join(__dirname + '../../../one.jpeg'));
 });
 
 app.get('/archive.html', function (req, res) {
     res.sendFile(path.join(__dirname + '/components/archive.html'));
+});
+
+app.get('/profile1.png', function (req, res) {
+    res.sendFile(path.join(__dirname + 'C:\Users\MY PC\Pictures\profile1.png'));
+});
+
+app.get('/binn.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/components/binn.html'));
 });
 
 
@@ -70,6 +82,23 @@ app.get('/displayNote.html', function (req, res) {
 app.get('/icons.html', function (req, res) {
     res.sendFile(path.join(__dirname + '/components/icons.html'));
 });
+
+
+
+app.get('/trythis.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/components/trythis.html'));
+});
+
+
+app.get('/trythis.css', function (req, res) {
+    res.sendFile(path.join(__dirname + '/components/trythis.css'));
+});
+
+
+
+
+
+
 
 var server = app.listen(process.env.PORT, () => {
     console.log("frontend server listening on port ", process.env.PORT);
